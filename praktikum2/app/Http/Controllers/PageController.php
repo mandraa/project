413597fa152c5b54b6fcc2c2864b6e9602d7320d
=mandraa <=andra.arema87@gmail.com>
2023-02-26 23:28:39 +0700

    Praktikum 1

---
+++ praktikum2/app/Http/Controllers/PageController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    //
+    public function index() {
+        return "Selamat Datang";
+    }
+
+    public function about() {
+        return "Juniar Andra P 2141720214";
+    }
+
+    public function articles(Request $request, $id) {
+        return "Halaman Artikel". $id;
+    }
+
+
+}
