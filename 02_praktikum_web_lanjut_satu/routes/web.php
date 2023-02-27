c91a3eb65dbd36f1b72942eaf6a27db47cf070e9
=mandraa <=andra.arema87@gmail.com>
2023-02-27 09:11:13 +0700

    Praktikum 1

--- 02_praktikum_web_lanjut_satu/routes/web.php
+++ 02_praktikum_web_lanjut_satu/routes/web.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return 'Selamat Datang';
+    return 'Selamat
+    Datang';
 });
 //Route::get('/test',[TestController::class,'index']);
 //Route::get('/', 'App\Http\Controllers\TestController@index');
